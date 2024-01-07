0989e2398638286d2e04b2ee2fd57cfde89e319b
Pi <yunji.seo@doomoolmori.com>
2024-01-07 16:22:26 +0900

    option 갖고놀기

--- deep-zio/src/main/scala/Init.scala
+++ deep-zio/src/main/scala/Init.scala
@@ -1,9 +1,18 @@
 import zio._
 
 object Init extends ZIOAppDefault{
+  case class  Cake(deco: Option[String], base: Option[String])
   override def run =
     for {
       _ <- zio.Console.printLine("start")
+      cake = Cake(Some("strawberry"), Some("plain"))
+      deco = cake.deco.filter(x => x.contains("strawberry")).map(y => y.concat("_\uD83C\uDF53"))
+      _ <- zio.Console.printLine(deco)
+
+      a = cake.base.map(x => x.->("a"))
+
+      _ <- zio.Console.printLine(a)
+
     }
     yield()
 }
