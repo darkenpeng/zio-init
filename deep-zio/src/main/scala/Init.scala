6d62d464fa24715feafbb60d78c453599fe5e342
Pi <yunji.seo@doomoolmori.com>
2024-01-21 20:44:08 +0900

    change patten match to map + if

--- deep-zio/src/main/scala/Init.scala
+++ deep-zio/src/main/scala/Init.scala
@@ -27,11 +27,17 @@ object Init extends ZIOAppDefault{
 
       cake = Cake(deco, None)
 
-      changedKirby = cake.deco match {
-        // 만약 준비된 데코가 딸기 데코면 커비가 행복해한다
-        case Some(d) if d.equals("strawberry") => kirby.changeStatus(cake)
-        // 데코가 준비되어 있지 않으면 커비가 실망한다
-        case None => kirby.changeStatus(cake)
+//      changedKirby = cake.deco match {
+//        // 만약 준비된 데코가 딸기 데코면 커비가 행복해한다
+//        case Some(d) if d.equals("strawberry") => kirby.changeStatus(cake)
+//        // 데코가 준비되어 있지 않으면 커비가 실망한다
+//        case None => kirby.changeStatus(cake)
+//      }
+
+      changedKirby = cake.deco.map {
+        d => if(d.equals("strawberry")) {
+          kirby.changeStatus(cake)
+        }
       }
 
       _ <- zio.Console.printLine(kirby)
